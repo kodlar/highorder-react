208f35b84be2c17db2de39290adec14f5001b1cf
kodlar <ogankeskiner@gmail.com>
2017-11-28 00:53:28 +0300

    refactor fixed

--- src/Components/HOC/Loader/LoaderHOC.js
+++ src/Components/HOC/Loader/LoaderHOC.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react'
 import './LoaderHOC.css';
 
+const isEmpty = (prop) => (
+    prop === null ||
+    prop === undefined ||
+    (prop.hasOwnProperty('length') && prop.length === 0) ||
+    (prop.constructor === Object && Object.keys(prop).length === 0)
+  );
+
 const LoaderHOC = (propName) => (WrappedComponent) => {
     return class LoaderHOC extends Component{
             render(){
                console.log('deger:',this.props[propName].length);
-               return (this.props[propName].length === 0 ? <div className="loadergif">Yükleniyor...</div> : <WrappedComponent {...this.props}/>)
+               return isEmpty(this.props[propName]) ? <div className="loadergif">Yükleniyor...</div> : <WrappedComponent {...this.props}/>
+               
             }
     }
 }
